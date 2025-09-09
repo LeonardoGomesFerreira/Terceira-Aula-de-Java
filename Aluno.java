7b51b6e5c589062e28123ac5b6c0e9b3a820cf27
Leonardo Gomes Ferreira <ferreiragomesleo26@gmail.com>
2025-09-09 20:33:12 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,17 @@
+public class Aluno {
+    private int ra;
+    private String nome;
+
+    public Aluno(int ra, String nome) {
+        this.ra = ra;
+        this.nome = nome;
+    }
+
+    public int getRa() {
+        return ra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
